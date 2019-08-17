879435c7a5a23deee9febd1d63431a4494200d30
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-17 20:08:25 +0200

    fix: small fix related to the rewards evaluation
    As the algorithm needs to learn to maximize the reward, the less time
    the simulation spends, the better the queue has been prioritized
    
    related: #3

--- src/gym_env.py
+++ src/gym_env.py
@@ -74,7 +74,7 @@ class OnBoardingEnvironment(gym.Env):
         # Run simulation
         while True:
             if len(self.queue) == 0 and self.plane.all_seated():
-                return None, self.rounds, True, None  # Corresponding to observation, reward, done, info
+                return None, -self.rounds, True, None  # Corresponding to observation, reward, done, info
 
             for agent in self.plane.passengers:  # update agents
                 agent.move()
