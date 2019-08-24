cafc120ebe20ed54fcbcfecdd620f03b10a5accc
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-08-24 22:50:23 +0200

    feat: Open's AI environment structure update
    Update of the skeleton and auxiliary files required for the proper
    function of the OpenAI's Gym environments
    
    related: #9

--- gym_plane_boarding/envs/__init__.py
+++ gym_plane_boarding/envs/__init__.py
@@ -1,1 +1,3 @@
-from gym_plane_boarding.envs.plane_boarding_env import PlaneBoardingEnvironment
+from gym_plane_boarding.envs.plane_boarding_b737_env import PlaneBoardingB737Environment
+from gym_plane_boarding.envs.plane_boarding_b747_env import PlaneBoardingB747Environment
+from gym_plane_boarding.envs.plane_boarding_a380_env import PlaneBoardingA380Environment
