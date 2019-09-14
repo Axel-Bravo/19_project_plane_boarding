6581930a9a1cb3254296c3e1cac232ea61e299ba
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-09-14 20:07:38 +0200

    feat: DQN agent implementation
    Reordering of the existing Replaybuffer testing procedure to accomodate
    the DQN algorithm
    
    close: #5

--- src/plane_boarding.py
+++ src/plane_boarding.py
@@ -3,28 +3,57 @@
 import gym
 import random
 import numpy as np
-from src.utils import ReplayBuffer
+import matplotlib.pyplot as plt
+from collections import deque
+from src.utils import ReplayBuffer, QAproximator
 
 from tensorflow.nn import log_softmax
 from tensorflow import convert_to_tensor, cast, float32
 
+# DRL Agent parameters
+min_num_experiences = 1000
+num_experiences = int(3e4)
 
-#%% Environment Initialization
+learn_every = 100
+learn_consecutively = 5
+
+
+#%% DQN Execution
 env = gym.make('gym_plane_boarding:b737-v0')
 buffer = ReplayBuffer()
 
-# Initial experiences on ReplayBuffer
-for _ in range(2500):
+rewards = []
+rewards_mean = deque(maxlen=100)
+
+q_network = QAproximator()
+q_target = QAproximator()
+q_target.copy_weights(network_to_copy=q_target)
+
+for i_exp in range(num_experiences):
     env.reset()
     passengers_queue = env.queue
 
-    # Generate random action
-    random_plane_queue_order = [i for i in range(1, env.observation_space.shape[0] + 1)]
-    random_plane_queue_order = np.array(log_softmax(cast(convert_to_tensor(random_plane_queue_order), float32)))
-    random.shuffle(random_plane_queue_order)
+    # Select Action
+    if i_exp < min_num_experiences:
+        # Generate random action
+        plane_queue_order = [i for i in range(1, env.observation_space.shape[0] + 1)]
+        plane_queue_order = np.array(log_softmax(cast(convert_to_tensor(plane_queue_order), float32)))
+        random.shuffle(plane_queue_order)
+    else:
+        pass
+        # TODO: to implement the E-greedy algorithm or plain greedy
 
-    observation, reward, done, info = env.step(action=random_plane_queue_order)
-    buffer.add(state=passengers_queue, action=random_plane_queue_order, reward=reward)
+    observation, reward, done, info = env.step(action=plane_queue_order)  # Run simulation
+    buffer.add(state=passengers_queue, action=plane_queue_order, reward=reward)  # Save experience
 
-# Agent learning
-    # TODO: implement initial DRL agent algorithm
+    rewards.append(reward)  # Save reward
+    rewards_mean.append(reward)  # Save reward mean
+
+    if i_exp < min_num_experiences:  # Agent average performance information
+        print("Agent average performance is: {}".format(round(np.array(rewards_mean).mean(), 2)))
+
+    if (i_exp < min_num_experiences) and (i_exp % learn_every == 0):  # Update Q-target Network
+        pass
+        # TODO: implementation of the Q-network learning and update procedures
+
+plt.plot(rewards)
